70d4e635c36eb3be87c87ff9d02c71dc297254da
Ckal <christof.kaller@gmail.com>
2026-01-03 10:55:39 +0100

    test: v1.1.0 - testing git credential store

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
-# Version: 1.0.9 - Testing fixed token authentication
+# Version: 1.1.0 - Testing git credential store auth
 launch_gradio_demo(SentimentAnalysisTool)
