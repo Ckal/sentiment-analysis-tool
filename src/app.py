d1286a0f972af527481d80f0ee409640a6181d5b
Ckal <christof.kaller@gmail.com>
2026-01-03 11:01:30 +0100

    test: v1.1.2 - testing without persisted credentials

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
-# Version: 1.1.1 - Testing PAT_TOKEN with quoted URL
+# Version: 1.1.2 - Testing with persist-credentials false
 launch_gradio_demo(SentimentAnalysisTool)
