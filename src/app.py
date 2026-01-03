8fe119a1f4bceba9d5c128871eab1353ab3f1479
Ckal <christof.kaller@gmail.com>
2026-01-03 11:07:39 +0100

    test: v1.1.4 - testing authenticated HF clone

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
-# Version: 1.1.3 - Testing lightweight HF deployment
+# Version: 1.1.4 - Testing authenticated HF clone
 launch_gradio_demo(SentimentAnalysisTool)
