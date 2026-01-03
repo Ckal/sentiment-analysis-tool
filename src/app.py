a6626122018a6e7e14029ad4c741fb3d834fe0ad
Ckal <christof.kaller@gmail.com>
2026-01-03 10:02:06 +0100

    test: v1.0.5 - test public repo push

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
-# Version: 1.0.4 - Testing deployment pipeline
+# Version: 1.0.5 - Testing public repo deployment
 launch_gradio_demo(SentimentAnalysisTool)
