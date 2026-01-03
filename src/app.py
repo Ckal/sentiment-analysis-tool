7f671f880a2f6362a24bb70663f2628504876807
Ckal <christof.kaller@gmail.com>
2026-01-03 09:11:20 +0100

    test: Update sentiment-analysis-tool v1.0.1

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
+# Version: 1.0.1 - Testing CI deployment
 launch_gradio_demo(SentimentAnalysisTool)
