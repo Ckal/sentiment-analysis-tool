7e4b986a792850a6116fbcfade05ec07c0e27efa
Ckal <christof.kaller@gmail.com>
2026-01-03 09:54:55 +0100

    test: sentiment-analysis-tool v1.0.4 - test full deployment

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
-# Version: 1.0.3 - Full CI/CD pipeline test
+# Version: 1.0.4 - Testing deployment pipeline
 launch_gradio_demo(SentimentAnalysisTool)
