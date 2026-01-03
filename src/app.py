063a870f9c02e6b00438dfa59c43bc6decbd9b1f
Ckal <christof.kaller@gmail.com>
2026-01-03 10:43:58 +0100

    test: v1.0.8 - testing deployment with secrets

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
-# Version: 1.0.7 - Testing with correct target parsing
+# Version: 1.0.8 - Testing with secrets configured
 launch_gradio_demo(SentimentAnalysisTool)
