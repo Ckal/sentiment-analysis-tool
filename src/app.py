2074276dc90ba30f3d5bdaae455c53688af4260b
Ckal <christof.kaller@gmail.com>
2026-01-03 09:45:51 +0100

    test: sentiment-analysis-tool v1.0.3 - final pipeline test

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
-# Version: 1.0.2 - Testing full CI/CD pipeline
+# Version: 1.0.3 - Full CI/CD pipeline test
 launch_gradio_demo(SentimentAnalysisTool)
