a7a5c38374596ab68693417b1b2f69aa85c95390
Ckal <christof.kaller@gmail.com>
2026-01-03 11:04:41 +0100

    test: v1.1.3 - testing lightweight deployment

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
-# Version: 1.1.2 - Testing with persist-credentials false
+# Version: 1.1.3 - Testing lightweight HF deployment
 launch_gradio_demo(SentimentAnalysisTool)
