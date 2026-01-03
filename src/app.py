d1cdcc3c8fe12e1ba3f25169ec0900ab4a879c7b
Ckal <christof.kaller@gmail.com>
2026-01-03 10:59:49 +0100

    test: v1.1.1 - testing PAT_TOKEN

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from sentiment_analysis import SentimentAnalysisTool
 
-# Version: 1.1.0 - Testing git credential store auth
+# Version: 1.1.1 - Testing PAT_TOKEN with quoted URL
 launch_gradio_demo(SentimentAnalysisTool)
